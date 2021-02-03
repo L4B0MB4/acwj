25c47aeb6fc719305a3d4718ac1b0f3b7ae56b2e
L4B0MB4 <metalfan65@gmail.com>
2021-02-03 17:25:04 +0100

    10_For_Loops base add

---
+++ 10_For_Loops/compiler.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+var AstOps = []string{"+", "-", "*", "/"}
+
+// Given an AST, interpret the
+// operators in it and return
+// a final value.
+func interpretAST(n *AstNode) string {
+	var leftval, rightval string
+
+	// Get the left and right sub-tree values
+	if n.left != nil {
+		leftval = interpretAST(n.left)
+	}
+	if n.right != nil {
+		rightval = interpretAST(n.right)
+	}
+
+	switch n.op {
+	case A_ADD:
+		return genAdd(leftval, rightval)
+	case A_SUBTRACT:
+		return genSub(leftval, rightval)
+	case A_MULTIPLY:
+		return genMul(leftval, rightval)
+	case A_DIVIDE:
+		return genDiv(leftval, rightval)
+	case A_EQ:
+		return genEq(leftval, rightval)
+	case A_NEQ:
+		return genNeq(leftval, rightval)
+	case A_GT:
+		return genGt(leftval, rightval)
+	case A_GE:
+		return genGe(leftval, rightval)
+	case A_LT:
+		return genLt(leftval, rightval)
+	case A_LE:
+		return genLe(leftval, rightval)
+	case A_INTLIT:
+		return genNumber(n)
+	case A_IDENT:
+		return genIdent(n)
+	case A_ASSIGNVAL:
+		return genAssignVal(n)
+	case A_ASSIGN:
+		return genAssign(leftval, rightval)
+	case A_PRINT:
+		return genPrint(leftval)
+	case A_IF:
+		return genIf(n)
+	case A_WHILE:
+		return genWhile(n)
+	case A_GLUETO:
+		return leftval + rightval + "\n"
+	default:
+		log.Fatalf("Unknown AST operator %d\n", n.op)
+		panic("Unknown AST operator")
+	}
+}
+
+func genWhile(node *AstNode) string {
+
+	whilehead := fmt.Sprintf("for %v {\n", interpretAST(node.left))
+	trueBody := fmt.Sprintf(" %v }\n", interpretAST(node.mid))
+	return whilehead + trueBody
+}
+
+func genIf(node *AstNode) string {
+
+	ifhead := fmt.Sprintf("if %v {\n", interpretAST(node.left))
+	trueBody := fmt.Sprintf(" %v }\n", interpretAST(node.mid))
+	falseBody := ""
+	if node.right != nil {
+		falseBody = fmt.Sprintf("else {\n %v }\n", interpretAST(node.right))
+	}
+	return ifhead + trueBody + falseBody
+}
---
+++ 10_For_Loops/global.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"os"
+)
+
+var Line int
+var Column int
+var Putback rune
+var InputFilePtr *os.File
+var InputFile *bufio.Reader
+var OutputFilePtr *os.File
+var OutputFile *bufio.Writer
+var TokenStr = []string{"+", "-", "*", "/", "intlit"}
+var T Token
+var OpPrecedence = []int{
+	0, 10, 10, // T_EOF, T_PLUS, T_MINUS
+	20, 20, // T_STAR, T_SLASH
+	30, 30, // T_EQ, T_NEQ
+	40, 40, 40, 40, // T_LT, T_GT, T_LE, T_GE
+}
+var LastScannedIdentifier string
+
+const (
+	T_EOF = iota
+	T_PLUS
+	T_MINUS
+	T_STAR
+	T_SLASH
+	T_EQ
+	T_NEQ
+	T_LT
+	T_GT
+	T_LE
+	T_GE
+	//no precedence
+	T_NEWLINE
+	T_INTLIT
+	T_PRINT
+	T_ASSIGN
+	T_LBRACE
+	T_RBRACE
+	T_LPAREN
+	T_RPAREN
+	//keywords
+	T_VAR
+	T_IDENT
+	T_IF
+	T_ELSE
+	T_WHILE
+)
+
+//Ast ops
+const (
+	A_EOF_PLACEHOLDER = iota //used to T_TOKEN == A_OP in getAstType
+	A_ADD
+	A_SUBTRACT
+	A_MULTIPLY
+	A_DIVIDE
+	A_EQ
+	A_NEQ
+	A_LT
+	A_GT
+	A_LE
+	A_GE
+	A_INTLIT
+	A_ASSIGNVAL
+	A_ASSIGN
+	A_IDENT
+	A_PRINT
+	A_GLUETO
+	A_IF
+	A_WHILE
+)
